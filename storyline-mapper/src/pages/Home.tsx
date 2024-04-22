ef270eb6c2644c5605bf4b9cedcc0efd55ae5ef1
jstnn818 <jstnn818@gmail.com>
2024-04-21 18:04:45 -0700

    homepage update

--- storyline-mapper/src/pages/Home.tsx
+++ storyline-mapper/src/pages/Home.tsx
@@ -1,15 +1,27 @@
-import world_map from '../images/world_map.jpg'
+import ExampleMap from '../images/example_map.jpg'
+import ExampleInfobox from '../images/example_infobox.png'
 
 const Home = () => {
     return (
-        <div>
-            <h1 className="font-bold text-2xl"> Storyline </h1>
-            <div className="flex justify-center">
-                <img src={world_map} alt="map" width="500" height="600"></img>
+        <div className="flex justify-center">
+            <div className="w-4/5">
+                {/* Title */}
+                <h1 className="font-bold text-2xl p-5"> Storyline Mapper </h1>
+
+                {/* Example Map */}
+                <div className="flex justify-center">
+                    <img src={ExampleMap} alt="map" width="500" height="600"></img>
+                </div>
+                <p className="p-5"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                
+                {/* Example Infobox */}
+                <div className="flex justify-center p-5">
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                    <img src={ExampleInfobox} alt="map"></img>
+                </div>
+                
             </div>
-            <p> This app does etc etc. </p>
         </div>
-        
     )
 }
 export default Home
