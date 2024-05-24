67922c9e1e26d90b185b733909aec526f52349df
jstnn818 <jstnn818@gmail.com>
2024-05-24 12:54:01 -0700

    Character Cards

--- storyline-mapper/src/pages/CharacterPage.tsx
+++ storyline-mapper/src/pages/CharacterPage.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import ImageFiller from 'react-image-filler'
 
 import { Character } from '../interfaces'
 
@@ -20,12 +21,19 @@ const CharacterPage = () => {
     }, [id])
 
     return (
-        <div className="flex justify-center mt-16">
-            <div className="w-4/6">
-                {/* Title */}
-                <h1 className="flex justify-center font-bold text-2xl p-5 text-red-700"> {character?.name} </h1>
+        <div className="w-screen flex justify-center mt-32">
+            <div className="w-4/6 shadow-lg">
 
-                <p className="p-5"> {character?.gender} </p>
+                <h1 className="flex justify-center font-bold text-2xl p-5 text-white bg-gray-700"> {character?.name} </h1>
+                
+                <div className="flex w-full">
+                    <ImageFiller width={200} height={200} />
+                    <div className="flex flex-col">
+                        <p className="p-5"> {character?.gender} </p>
+                        <p className="p-5"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     )
